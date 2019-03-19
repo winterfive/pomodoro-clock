22d3a51f4519a721f348551cc7b3a5dc34833f85
Lee <winterfive@users.noreply.github.com>
2019-03-19 15:54:29 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  var buzzer = $("#buzzer");
+  var count = parseInt($("session-length").html());
+  console.log("count: " + count);
+  $(".reset").hide();
+  
+  $("#session-decrement").click(function()){
+    count: -= 1;
+    $("#session-length").html(count);
+  }                          
+});
