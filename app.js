a52e5746d4e4d12a55fc4fe4b5af1e6b3889481b
Lee <winterfive@users.noreply.github.com>
2019-03-19 16:01:56 -0400

    Coded out session-decrement function

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 $(document).ready(function() {
   var buzzer = $("#buzzer");
-  var count = parseInt($("session-length").html());
+  var count = parseInt($("#session-length").html());
   console.log("count: " + count);
   $(".reset").hide();
   
-  $("#session-decrement").click(function()){
-    count: -= 1;
+  $("#session-decrement").click(function(){
+    count -= 1;
     $("#session-length").html(count);
-  }                          
+    console.log("session is: " + count);
+  })
 });
+
+  
+  
