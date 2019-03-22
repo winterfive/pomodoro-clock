90890e9ff1d476cfafcf4208507499b8338c202c
Lee <winterfive@users.noreply.github.com>
2019-03-22 11:17:08 -0400

    Changed BreakTime label assignment to Break Time in runTimer()

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ $(document).ready(function() {
           clearInterval(startSession);
           sessCount = parseInt($("#session-length").html()) * 60;
           breakCount = parseInt($("#break-length").html()) * 60;
-          $("#timer-label").html("BreakTime: <span id='time-left'></span>");
+          $("#timer-label").html("Break Time: <span id='time-left'></span>");
           startBreak = setInterval( function() { runTimer(); }, 1000);
           console.log("got beyound startBreak");
           isSessionRunning = false;
